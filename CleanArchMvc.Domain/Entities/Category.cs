40a637db734cd980724143585ada34133d48a3d9
ANDREOGP33 <andregasparoni690@gmail.com>
2024-01-04 13:56:02 -0300

    Enriquecendo o modelo de domínio criado - I

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,32 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private  set; }
+        public Category(String name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExeceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(String name)
+        {
+            DomainExeceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+
+            DomainExeceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
---
+++ CleanArchMvc.Domain/Validation/DomainExeceptionValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Validation
+{
+    public class DomainExeceptionValidation : Exception
+    {
+        public DomainExeceptionValidation(string error) : base(error)
+        {           
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExeceptionValidation(error);
+            }
+        }
+    }
+}
