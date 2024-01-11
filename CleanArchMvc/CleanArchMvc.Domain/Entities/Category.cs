1038adc0deb3f3b8b2ec4dbff9ccfa64ddb89ebe
ANDREOGP33 <andregasparoni690@gmail.com>
2024-01-11 12:45:27 -0300

    Commit para analise

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            Name = name;
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Ivalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvcInfra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
